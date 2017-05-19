93029c8cfeb312f857f124576c0107ee3b7e2560
Dawid Szydlo <dawid.szydlo@spot.pl>
2017-05-19 19:31:53 +0200

    more kotlin style code

--- src/main/kotlin/pl/gumyns/java_code_generator/model/ClassDef.kt
+++ src/main/kotlin/pl/gumyns/java_code_generator/model/ClassDef.kt
@@ -1,3 +1,6 @@
 package pl.gumyns.java_code_generator.model
 
-data class ClassDef(val name: String, val variables: List<VariableDef>)
+data class ClassDef(
+        val name: String,
+        val variables: List<VariableDef>
+)
--- src/main/kotlin/pl/gumyns/java_code_generator/model/VariableDef.kt
+++ src/main/kotlin/pl/gumyns/java_code_generator/model/VariableDef.kt
@@ -1,3 +1,8 @@
 package pl.gumyns.java_code_generator.model
 
-data class VariableDef(val type: String, val name: String, val value: String?, val values: List<String>?)
+data class VariableDef(
+        val type: String,
+        val name: String,
+        val value: String?,
+        val values: List<String>?
+)
